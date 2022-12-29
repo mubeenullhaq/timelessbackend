781faf4180579fd28819880dd8bf6976e05f065f
mubeenullhaqblkdev <mubeenul.haq@blocksdev.com>
2022-12-30 04:19:21 +0500

    middlewares added

--- startup/routes.js
+++ startup/routes.js
@@ -1,11 +1,14 @@
 const xmlparser = require("express-xml-bodyparser");
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const timeless = require("../routes/timeless");
 
 module.exports = function (app) {
   app.use(xmlparser());
   app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cors());
   app.options("*", cors());
 
