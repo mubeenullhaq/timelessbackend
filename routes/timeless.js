4ab34a987133ff78f8e4fa03c282c1f2e7bb05ac
mubeenullhaqblkdev <mubeenul.haq@blocksdev.com>
2022-12-30 04:18:19 +0500

    post route hardcoded for testing the route and not hit OPEN AI and D-ID

--- routes/timeless.js
+++ routes/timeless.js
@@ -3,38 +3,35 @@ const {
   openAIreq,
   d_idPostRequest,
   d_idGetRequest,
+  isResultUrlAvailable,
 } = require("../routes/helper");
 const config = require("config");
 let router = express.Router();
-
+console.log(config.get("OPENAI_API_KEY"));
 //Create route
 router.post("/", async (req, res) => {
   try {
     console.log(config.get("OPENAI_API_KEY"));
-    console.log(req.body.text);
-    let OpenAIres = await openAIreq(req.body.text);
-    console.log(OpenAIres);
-    const openAIresponse = OpenAIres.data.choices[0].text;
-    //Call D-ID API end point with source URL to create video
-    let d_idresponse = await d_idPostRequest(
-      openAIresponse,
-      "Diego",
-      "0.8",
-      "https://asongwall.com/wp-content/uploads/2022/12/van-gogh.png"
-    );
-    let d_idVideoObj = await d_idGetRequest(d_idresponse.body.id);
+    console.log(req.body.userInput);
+    // let OpenAIres = await openAIreq(req.body.userInput);
+    // console.log(OpenAIres);
+    // const openAIresponse = OpenAIres.data.choices[0].text;
+    // //Call D-ID API end point with source URL to create video
+    // let d_idresponse = await d_idPostRequest(
+    //   openAIresponse,
+    //   "Diego",
+    //   "0.8",
+    //   "https://asongwall.com/wp-content/uploads/2022/12/van-gogh.png"
+    // );
+    let d_idVideoObj = await d_idGetRequest("tlk_JVEW22GZLFuh-ECk3EwRY");
+    //let d_idVideoObj = await d_idGetRequest(d_idresponse.body.id);
     d_idVideoObj = JSON.parse(d_idVideoObj);
     console.log(d_idVideoObj.result_url);
-    // if (!d_idVideoObj.result_url) {
-    //   let interval = setInterval(async () => {
-    //     let d_idVideoObj = await d_idGetRequest(d_idresponse.body.id);
-    //     d_idVideoObj.result_url = JSON.parse(d_idVideoObj.result_url);
-    //     if (d_idVideoObj.result_url) {
-    //       resolv(d_idVideoObj.result_url);
-    //       clearInterval(interval);
-    //     }
-    //   }, 1000);
-    // }
+    if (!d_idVideoObj.result_url) {
+      d_idVideoObj.result_url = await isResultUrlAvailable(
+        "tlk_JVEW22GZLFuh-ECk3EwRY"
+      );
+    }
     return res.status(200).send({ result_url: d_idVideoObj.result_url });
   } catch (e) {
     console.log(e);
@@ -45,6 +42,7 @@ router.post("/", async (req, res) => {
 //Test route
 router.post("/test", async (req, res) => {
   try {
+    console.log(req.body.userInput);
     console.log(req.body);
     return res.status(200).send(req.body);
   } catch (e) {
